7c52c8b4c27314c7db1fd3a369a3b0ab75ea985f
boomkim <bhk3177@gmail.com>
2020-08-28 20:19:29 +0900

    회원가입, 헬스체크 구현

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+/* health check */
+router.get('/hello', function(req, res, next) {
+  res.sendStatus(200);
+});
+
 module.exports = router;
