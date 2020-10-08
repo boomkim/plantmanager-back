8bfa128211e3e155a7759bf44eb3be76825e5b3b
boomkim <bhk3177@gmail.com>
2020-10-08 19:10:50 +0900

    구현: GET /api/signedurl

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var mongoose = require('mongoose');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mongoose = require('mongoose');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var authRouter = require('./routes/auth');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const authRouter = require('./routes/auth');
+const apiRouter = require('./routes/api');
 
 const config = require('./config');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,7 +21,7 @@ app.set('view engine', 'pug');
 app.set('jwt-secret',config.secret);
 
 // db setup 
-var db = mongoose.connection;
+const db = mongoose.connection;
 db.on('error', console.error);
 db.once('open', function() {
   console.log("Connected to MongoDB Server");
@@ -38,6 +39,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/auth', authRouter);
+app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/api.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router = express.Router();
+const AWS = require('aws-sdk');
+const authMiddleware = require('../middleware/auth');
+const s3 = new AWS.S3({signatureVersion: 'v4', region: 'ap-northeast-2'});
+const { v4: uuidv4 } = require('uuid');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.use('/signedurl', authMiddleware);
+router.get('/signedurl', function(req, res, next) {
+  const userid = req.decoded.userid;
+  console.log(userid);
+  const key = 'userplants/' + userid + '/' + uuidv4();
+  const params = {
+    Bucket: 'plant-manager',
+    Key: key
+  }
+  const signedUrl = s3.getSignedUrl('putObject',params);
+  console.log(signedUrl);
+  res.send({signedUrl});
+});
+
+/* health check */
+router.get('/hello', function(req, res, next) {
+  res.sendStatus(200);
+});
+
+module.exports = router;
